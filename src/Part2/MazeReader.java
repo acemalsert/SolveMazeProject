80a6c343bde7fd028cb4ae5220c1b269a676494b
Ahmet Cemal Sert <55139481+acemalsert@users.noreply.github.com>
2024-10-03 10:06:29 +0300

    Update MazeReader.java

--- src/Part2/MazeReader.java
+++ src/Part2/MazeReader.java
@@ -16,126 +16,49 @@ import java.io.IOException;
 
 public class MazeReader {
 
-
-    public static Graph readMaze(String fileName, Graph graph, Maze maze){
-        String [] parameters;
+    public static Graph readMaze(String fileName, Graph graph, Maze maze) {
+        String[] parameters;
         maze = new Maze();
 
+        try {
+            File file = new File(fileName);    // creates a new file instance
+            FileReader fileReader = new FileReader(file);   // reads the file
+            BufferedReader bufferedReader = new BufferedReader(fileReader);  // creates a buffering character input stream
 
-        try
-        {
-            File file=new File(fileName);    //creates a new file instance
-            FileReader fileReader = new FileReader(file);   //reads the file
-            BufferedReader bufferedReader = new BufferedReader(fileReader);  //creates a buffering character input stream
             String line;
-            int count = 0;
-            while((line=bufferedReader.readLine())!=null)
-            {
-                if(count == 0){
-                    graph = new Graph(Integer.parseInt(line)); // Gives the size of the graph and initializes it
-                    count ++;
+            int lineNumber = 0;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                // Ignore empty lines
+                if (line.trim().isEmpty()) {
+                    continue;
                 }
 
-                else if (count==1){
-                    parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
-                }
-                else if(count==2){
-                    parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
-                }
+                lineNumber++;
 
-                else if(count==3){
+                if (lineNumber == 1) {
+                    // First line: initialize the graph with the number of vertices
+                    graph = new Graph(Integer.parseInt(line.trim()));
+                } else if (lineNumber <= 8) {
+                    // Next 8 lines: add rooms to the maze
                     parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
-                }
-
-                else if(count==4){
+                    maze.getRooms().add(new Room(parameters[0], Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2])));
+                } else if (line.equals("-1 -1")) {
+                    // Terminate on encountering "-1 -1"
+                    break;
+                } else {
+                    // Remaining lines: add edges to the graph
                     parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
+                    graph.addEdge(Integer.parseInt(parameters[0]), Integer.parseInt(parameters[1]));
                 }
-
-                else if(count==5){
-                    parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
-                }
-
-                else if(count==6){
-                    parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
-                }
-
-                else if(count==7){
-                    parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
-                }
-
-                else if(count==8){
-                    parameters = line.split(" ");
-                    maze.getRooms().add(new Room(parameters[0],Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2])));
-                    count++;
-                }
-                else if(line.isEmpty()){
-                   continue;
-                }
-                else if(count==9){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-                else if(count==10){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-                else if(count==11){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-                else if(count==12){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-                else if(count==13){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-                else if(count==14){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-                else if(count==15){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-                else if(count==16){
-                    parameters = line.split(" ");
-                    graph.addEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1]));
-                    count++;
-                }
-
-                else if(line.equals("-1 -1")) break;
-
             }
-            fileReader.close();    //closes the stream
+
+            bufferedReader.close();    // close the stream
             System.out.println(graph.toString());
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
         return graph;
     }
 }
